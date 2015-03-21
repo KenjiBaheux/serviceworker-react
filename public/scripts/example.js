e5ef915ef18351fd3cc2aa2923938cf8114b6338
Lewis Cowper <lewis.cowper@wandisco.com>
2015-03-21 12:25:43 +0000

    initial commit

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,36 +1,22 @@
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation purposes only.
- * Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-var converter = new Showdown.converter();
-
-var Comment = React.createClass({
-  render: function() {
-    var rawMarkup = converter.makeHtml(this.props.children.toString());
+var Disruption = React.createClass({
+  render: function () {
     return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
+      <div className="disruption">
+        <h2 className="disruptionText">
+          {this.props.text}
         </h2>
-        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+        <span />
       </div>
     );
   }
 });
 
-var CommentBox = React.createClass({
-  loadCommentsFromServer: function() {
+var DisruptionBox = React.createClass({
+  loadDisruptionsFromServer: function() {
     $.ajax({
       url: this.props.url,
-      dataType: 'json',
+      dataType: 'xml',
+      method: 'GET',
       success: function(data) {
         this.setState({data: data});
       }.bind(this),
@@ -39,89 +25,41 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
-  handleCommentSubmit: function(comment) {
-    var comments = this.state.data;
-    comments.push(comment);
-    this.setState({data: comments}, function() {
-      // `setState` accepts a callback. To avoid (improbable) race condition,
-      // `we'll send the ajax request right after we optimistically set the new
-      // `state.
-      $.ajax({
-        url: this.props.url,
-        dataType: 'json',
-        type: 'POST',
-        data: comment,
-        success: function(data) {
-          this.setState({data: data});
-        }.bind(this),
-        error: function(xhr, status, err) {
-          console.error(this.props.url, status, err.toString());
-        }.bind(this)
-      });
-    });
-  },
   getInitialState: function() {
     return {data: []};
   },
   componentDidMount: function() {
-    this.loadCommentsFromServer();
-    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    this.loadDisruptionsFromServer();
+    setInterval(this.loadDisruptionsFromServer, this.props.pollInterval);
   },
   render: function() {
     return (
-      <div className="commentBox">
-        <h1>Comments</h1>
-        <CommentList data={this.state.data} />
-        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+      <div className="disruptionBox">
+        <h1>Disruptions</h1>
+        <DisruptionList data={this.state.data} />
       </div>
     );
   }
 });
 
-var CommentList = React.createClass({
+var DisruptionList = React.createClass({
   render: function() {
-    var commentNodes = this.props.data.map(function(comment, index) {
+    var disruptionNodes = this.props.data.map(function(disruption, index) {
       return (
-        // `key` is a React-specific concept and is not mandatory for the
-        // purpose of this tutorial. if you're curious, see more here:
-        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
-        <Comment author={comment.author} key={index}>
-          {comment.text}
-        </Comment>
+        <Disruption station={disruption.station} key={index}>
+          {disruption.text}
+        </Disruption>
       );
     });
     return (
-      <div className="commentList">
-        {commentNodes}
+      <div className="disruptionList">
+        {disruptionNodes}
       </div>
-    );
-  }
-});
-
-var CommentForm = React.createClass({
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var author = this.refs.author.getDOMNode().value.trim();
-    var text = this.refs.text.getDOMNode().value.trim();
-    if (!text || !author) {
-      return;
-    }
-    this.props.onCommentSubmit({author: author, text: text});
-    this.refs.author.getDOMNode().value = '';
-    this.refs.text.getDOMNode().value = '';
-  },
-  render: function() {
-    return (
-      <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Your name" ref="author" />
-        <input type="text" placeholder="Say something..." ref="text" />
-        <input type="submit" value="Post" />
-      </form>
     );
   }
 });
 
 React.render(
-  <CommentBox url="comments.json" pollInterval={2000} />,
+  <DisruptionBox url='http://cors-io.herokuapp.com/cloud.tfl.gov.uk/TrackerNet/stationStatus' pollInterval={20000} />,
   document.getElementById('content')
 );
