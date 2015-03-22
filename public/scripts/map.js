10124ea6d2f30e52f141f5436d04b19e56a61211
Lewis Cowper <lewis.cowper@wandisco.com>
2015-03-22 23:16:20 +0000

    added logos and kml request instead of /data folder

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -3,15 +3,40 @@ var loader = document.getElementById('loading');
 
 startLoading();
 
-var map = L.mapbox.map('map', 'lewiscowper.lgjh3npk').setView([51.5182, -0.1368], 13);
-
-var stationData = L.mapbox.featureLayer().loadURL('data/stations.json')
-  .setFilter(function() {
-    // This should be where I do my check to display stations
-    return true;
-  })
-  .addTo(map)
+var map = L.mapbox.map('map', 'lewiscowper.lgjh3npk')
+  .setView([51.5182, -0.1368], 13)
   .on('ready', finishedLoading);
+
+var stationData = omnivore.kml('https://cors-anywhere.herokuapp.com/data.tfl.gov.uk/tfl/syndication/feeds/stations.kml?app_id=bd960a5c&app_key=b8b676fb485340bb1a9352b3d9b44617')
+  .on('ready', function(layer) {
+    this.eachLayer(function (marker) {
+      if (marker.toGeoJSON().properties.styleUrl === '#dlrStyle') {
+        console.log(marker.toGeoJSON());
+        marker.toGeoJSON().properties.image = 'https://www.tfl.gov.uk/cdn/static/cms/images/logos/dlr-partner.png';
+        marker.setIcon(L.mapbox.marker.icon({
+            'marker-color': '#009999'
+        }));
+      } else if (marker.toGeoJSON().properties.styleUrl === '#tubeStyle') {
+        marker.toGeoJSON().properties.image = 'https://www.tfl.gov.uk/tfl-global/images/syndication/roundel-tube.png';
+        marker.setIcon(L.mapbox.marker.icon({
+            'marker-color': '#CC3333'
+        }));
+      } else {
+        marker.setIcon(L.mapbox.marker.icon({}));
+      }
+
+      var popupContent =  '<img src="' + marker.toGeoJSON().properties.image + '" />' +
+                            marker.toGeoJSON().properties.name +
+                          '</a>';
+
+      marker.bindPopup(popupContent, {
+        closeButton: false,
+        minWidth: 200,
+        maxWidth: 300
+      });
+    });
+  })
+  .addTo(map);
 
 function startLoading() {
     loader.className = '';
