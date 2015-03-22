27e67b89b2d7351627711791c777e860f4e1d851
Lewis Cowper <lewis.cowper@wandisco.com>
2015-03-22 18:54:25 +0000

    added loading animation

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,4 +1,7 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibGV3aXNjb3dwZXIiLCJhIjoiaGl1T2hNdyJ9.laORI6ZYpiizWtPfceRMfQ';
+var loader = document.getElementById('loading');
+
+startLoading();
 
 var map = L.mapbox.map('map', 'lewiscowper.lgjh3npk').setView([51.5182, -0.1368], 13);
 
@@ -7,4 +10,21 @@ var stationData = L.mapbox.featureLayer().loadURL('data/stations.json')
     // This should be where I do my check to display stations
     return true;
   })
-  .addTo(map);
+  .addTo(map)
+  .on('ready', finishedLoading);
+
+function startLoading() {
+    loader.className = '';
+}
+
+function finishedLoading() {
+    // first, toggle the class 'done', which makes the loading screen
+    // fade out
+    loader.className = 'done';
+    setTimeout(function() {
+        // then, after a half-second, add the class 'hide', which hides
+        // it completely and ensures that the user can interact with the
+        // map again.
+        loader.className = 'hide';
+    }, 500);
+}
